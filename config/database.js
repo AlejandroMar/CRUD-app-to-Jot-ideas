83bae0a9a00322a56b90f7c78680833c2aea8090
Alejo <Alejo@Alejos-MacBook-Pro.local>
2018-07-09 11:34:43 +0200

    Update name for app and data-base

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 if (process.env.NODE_ENV === "production") {
-    module.exports = {mongoURI: `mongodb://${process.env.DB_USER}:${process.env.BD_KEY}@ds111370.mlab.com:11370/crud-node-app`}
+    module.exports = {mongoURI: `mongodb://${process.env.DB_USER}:${process.env.BD_KEY}@ds131711.mlab.com:31711/que-nota`}
 } else {
     module.exports = {mongoURI: "mongodb://localhost:27017/vidjot-dev"}
 }
